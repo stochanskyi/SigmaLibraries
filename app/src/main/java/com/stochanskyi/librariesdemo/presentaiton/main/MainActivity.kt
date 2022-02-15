326d3cc1671e24290bda3137f0aa6cab35f6cb50
Marian Stochanskyi <stochan.andr@gmail.com>
2022-02-15 18:33:47 +0200

    Implement splash screen

--- app/src/main/java/com/stochanskyi/librariesdemo/presentaiton/main/MainActivity.kt
+++ app/src/main/java/com/stochanskyi/librariesdemo/presentaiton/main/MainActivity.kt
@@ -2,13 +2,14 @@ package com.stochanskyi.librariesdemo.presentaiton.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.stochanskyi.librariesdemo.R
 import com.stochanskyi.librariesdemo.databinding.ActivityMainBinding
 import com.stochanskyi.librariesdemo.presentaiton.imageloading.ImageLoadersFragment
-import com.stochanskyi.librariesdemo.presentaiton.simplecall.SimpleCallFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
