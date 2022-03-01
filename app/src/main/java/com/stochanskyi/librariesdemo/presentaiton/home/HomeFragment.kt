9d2406863f7a7d19ea2da0b07c647f76465c6b75
Marian Stochanskyi <stochan.andr@gmail.com>
2022-03-01 14:14:51 +0200

    Implement home screen

---
+++ app/src/main/java/com/stochanskyi/librariesdemo/presentaiton/home/HomeFragment.kt
@@ -0,0 +1,28 @@
+package com.stochanskyi.librariesdemo.presentaiton.home
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.stochanskyi.librariesdemo.R
+import com.stochanskyi.librariesdemo.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        FragmentHomeBinding.bind(view).run {
+            initListeners(this)
+        }
+    }
+
+    private fun initListeners(binding: FragmentHomeBinding) = with(binding) {
+        simpleCallButton.setOnClickListener {
+            findNavController().navigate(R.id.fragment_simple_call)
+        }
+        imageLoadingButton.setOnClickListener {
+            findNavController().navigate(R.id.fragment_image_loaders)
+        }
+    }
+
+}
--- app/src/main/java/com/stochanskyi/librariesdemo/presentaiton/main/MainActivity.kt
+++ app/src/main/java/com/stochanskyi/librariesdemo/presentaiton/main/MainActivity.kt
@@ -3,25 +3,15 @@ package com.stochanskyi.librariesdemo.presentaiton.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.stochanskyi.librariesdemo.R
 import com.stochanskyi.librariesdemo.databinding.ActivityMainBinding
-import com.stochanskyi.librariesdemo.presentaiton.imageloading.ImageLoadersFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        if (savedInstanceState != null) {
-            return
+        ActivityMainBinding.inflate(layoutInflater).let {
+            setContentView(it.root)
         }
-
-        supportFragmentManager.beginTransaction()
-            .add(R.id.fragment_container, ImageLoadersFragment.newInstance())
-            .commit()
     }
-
 }
