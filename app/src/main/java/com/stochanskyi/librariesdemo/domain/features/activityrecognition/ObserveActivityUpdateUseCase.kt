a396a46ac9d99032eec4b01e34c5964622f9edec
Marian Stochanskyi <stochan.andr@gmail.com>
2022-03-03 00:03:07 +0200

    Add activity update and stop update use case

---
+++ app/src/main/java/com/stochanskyi/librariesdemo/domain/features/activityrecognition/ObserveActivityUpdateUseCase.kt
@@ -0,0 +1,19 @@
+package com.stochanskyi.librariesdemo.domain.features.activityrecognition
+
+import com.stochanskyi.librariesdemo.data.repository.activityrecognition.ActivityRecognitionRepository
+import com.stochanskyi.librariesdemo.data.repository.activityrecognition.models.ActivityUpdate
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+interface ObserveActivityUpdateUseCase {
+    operator fun invoke(): Flow<ActivityUpdate>
+}
+
+class ObserveActivityUpdateUseCaseImpl @Inject constructor(
+    private val activityRecognitionRepository: ActivityRecognitionRepository
+) : ObserveActivityUpdateUseCase {
+    override fun invoke(): Flow<ActivityUpdate> {
+        return activityRecognitionRepository.getActivityUpdateFlow()
+    }
+
+}
