e89131444f98e272b1b66efc5ce304900502704f
Marian Stochanskyi <stochan.andr@gmail.com>
2022-03-08 18:11:10 +0200

    Implement UI for activity recognition

--- app/src/main/java/com/stochanskyi/librariesdemo/presentaiton/activityrecognition/activityupdate/ActivityUpdateFragment.kt
+++ app/src/main/java/com/stochanskyi/librariesdemo/presentaiton/activityrecognition/activityupdate/ActivityUpdateFragment.kt
@@ -5,9 +5,12 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.stochanskyi.librariesdemo.R
 import com.stochanskyi.librariesdemo.app.appComponent
 import com.stochanskyi.librariesdemo.databinding.FragmentActivityUpdateBinding
+import com.stochanskyi.librariesdemo.presentaiton.activityrecognition.activityupdate.adapter.ActivityUpdateEventAdapter
 import com.stochanskyi.librariesdemo.presentaiton.utils.ViewModelFactory
 import javax.inject.Inject
 
@@ -25,9 +28,29 @@ class ActivityUpdateFragment : Fragment(R.layout.fragment_activity_update) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         FragmentActivityUpdateBinding.bind(view).run {
-            startStopButton.setOnClickListener {
-                viewModel.startOrStopUpdate()
-            }
+            initViews(this)
+            initObservers(this)
+        }
+    }
+
+    private fun initViews(binding: FragmentActivityUpdateBinding) = with(binding) {
+        eventsRecyclerView.adapter = ActivityUpdateEventAdapter()
+        eventsRecyclerView.layoutManager = LinearLayoutManager(context)
+        eventsRecyclerView.addItemDecoration(
+            DividerItemDecoration(
+                context,
+                DividerItemDecoration.VERTICAL
+            )
+        )
+
+        startStopButton.setOnClickListener {
+            viewModel.startOrStopUpdate()
+        }
+    }
+
+    private fun initObservers(binding: FragmentActivityUpdateBinding) {
+        viewModel.activityEventsLiveData.observe(viewLifecycleOwner) {
+            (binding.eventsRecyclerView.adapter as? ActivityUpdateEventAdapter)?.submitList(it)
         }
     }
 
---
+++ app/src/main/java/com/stochanskyi/librariesdemo/presentaiton/activityrecognition/activityupdate/adapter/ActivityUpdateEventAdapter.kt
@@ -0,0 +1,21 @@
+package com.stochanskyi.librariesdemo.presentaiton.activityrecognition.activityupdate.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import com.stochanskyi.librariesdemo.presentaiton.activityrecognition.activityupdate.viewdata.ActivityUpdateEventViewData
+
+class ActivityUpdateEventAdapter : ListAdapter<ActivityUpdateEventViewData, ActivityUpdateEventViewHolder>(
+    ActivityUpdateEventItemCallback()
+) {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ActivityUpdateEventViewHolder {
+        return ActivityUpdateEventViewHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: ActivityUpdateEventViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
