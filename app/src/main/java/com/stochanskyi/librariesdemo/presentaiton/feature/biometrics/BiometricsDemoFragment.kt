b6a3735fa875ae75a06ca8b734eed5d083b4a315
Marian Stochanskyi <stochan.andr@gmail.com>
2022-04-20 16:03:50 +0300

    Implement biometrics demo UI

--- app/src/main/java/com/stochanskyi/librariesdemo/presentaiton/feature/biometrics/BiometricsDemoFragment.kt
+++ app/src/main/java/com/stochanskyi/librariesdemo/presentaiton/feature/biometrics/BiometricsDemoFragment.kt
@@ -1,10 +1,13 @@
 package com.stochanskyi.librariesdemo.presentaiton.feature.biometrics
 
 import android.content.Context
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.stochanskyi.librariesdemo.R
 import com.stochanskyi.librariesdemo.app.appComponent
+import com.stochanskyi.librariesdemo.databinding.FragmentBiometricsDemoBinding
 import com.stochanskyi.librariesdemo.presentaiton.utils.ViewModelFactory
 import javax.inject.Inject
 
@@ -19,5 +22,27 @@ class BiometricsDemoFragment : Fragment(R.layout.fragment_biometrics_demo) {
         super.onAttach(context)
 
         context.appComponent().inject(this)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        FragmentBiometricsDemoBinding.bind(view).run {
+            initViews(this)
+            initObservers(this)
+        }
+    }
+
+    private fun initViews(binding: FragmentBiometricsDemoBinding): Unit = with(binding) {
+        saveButton.setOnClickListener {
+            viewModel.saveData(dataEditText.text?.toString() ?: "")
+        }
+        restoreButton.setOnClickListener {
+            viewModel.restoreData()
+        }
+    }
+
+    private fun initObservers(binding: FragmentBiometricsDemoBinding) = with(binding) {
+        viewModel.restoredLiveData.observe(viewLifecycleOwner) {
+            dataEditText.setText(it)
+        }
     }
 }
