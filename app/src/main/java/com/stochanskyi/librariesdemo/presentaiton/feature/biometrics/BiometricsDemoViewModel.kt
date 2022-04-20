3c6943407a99699f58acbd89cf34209691365a61
Marian Stochanskyi <stochan.andr@gmail.com>
2022-04-20 16:03:50 +0300

    Implement biometrics demo UI

--- app/src/main/java/com/stochanskyi/librariesdemo/presentaiton/feature/biometrics/BiometricsDemoViewModel.kt
+++ app/src/main/java/com/stochanskyi/librariesdemo/presentaiton/feature/biometrics/BiometricsDemoViewModel.kt
@@ -1,6 +1,20 @@
 package com.stochanskyi.librariesdemo.presentaiton.feature.biometrics
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import javax.inject.Inject
 
-class BiometricsDemoViewModel @Inject constructor() : ViewModel()
+class BiometricsDemoViewModel @Inject constructor() : ViewModel() {
+
+    private val _restoredLiveData = MutableLiveData<String>()
+    val restoredLiveData: LiveData<String> = _restoredLiveData
+
+    fun saveData(data: String) {
+
+    }
+
+    fun restoreData() {
+
+    }
+}
