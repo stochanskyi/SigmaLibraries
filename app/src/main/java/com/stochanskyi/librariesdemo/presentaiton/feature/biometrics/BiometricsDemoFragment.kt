794b811b59af203e8b988fcfbe70d39ec0d2e74e
Marian Stochanskyi <stochan.andr@gmail.com>
2022-04-20 16:46:40 +0300

    Implement UI logic for authenticating encoding/decoding cipher

--- app/src/main/java/com/stochanskyi/librariesdemo/presentaiton/feature/biometrics/BiometricsDemoFragment.kt
+++ app/src/main/java/com/stochanskyi/librariesdemo/presentaiton/feature/biometrics/BiometricsDemoFragment.kt
@@ -3,12 +3,17 @@ package com.stochanskyi.librariesdemo.presentaiton.feature.biometrics
 import android.content.Context
 import android.os.Bundle
 import android.view.View
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
+import androidx.biometric.BiometricPrompt
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.stochanskyi.librariesdemo.R
 import com.stochanskyi.librariesdemo.app.appComponent
 import com.stochanskyi.librariesdemo.databinding.FragmentBiometricsDemoBinding
 import com.stochanskyi.librariesdemo.presentaiton.utils.ViewModelFactory
+import javax.crypto.Cipher
 import javax.inject.Inject
 
 class BiometricsDemoFragment : Fragment(R.layout.fragment_biometrics_demo) {
@@ -17,6 +22,8 @@ class BiometricsDemoFragment : Fragment(R.layout.fragment_biometrics_demo) {
     lateinit var viewModelFactory: ViewModelFactory
 
     private val viewModel: BiometricsDemoViewModel by viewModels { viewModelFactory }
+
+    private val biometricManager: BiometricManager by lazy { BiometricManager.from(requireContext()) }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -44,5 +51,52 @@ class BiometricsDemoFragment : Fragment(R.layout.fragment_biometrics_demo) {
         viewModel.restoredLiveData.observe(viewLifecycleOwner) {
             dataEditText.setText(it)
         }
+        viewModel.authenticateEncryptCipherLiveData.observe(viewLifecycleOwner) { cipher ->
+            showEncryptCipherBiometricPrompt(cipher)
+        }
+        viewModel.authenticateDecryptCipherLiveData.observe(viewLifecycleOwner) { cipher ->
+            showDecodeCipherBiometricPrompt(cipher)
+        }
+    }
+
+    private fun showEncryptCipherBiometricPrompt(cipher: Cipher) {
+        showBiometricPrompt(cipher) { authenticatedCipher ->
+            authenticatedCipher?.let { viewModel.encodeData(it) }
+        }
+    }
+
+    private fun showDecodeCipherBiometricPrompt(cipher: Cipher) {
+        showBiometricPrompt(cipher) { authenticatedCipher ->
+            authenticatedCipher?.let { viewModel.decodeData(it) }
+        }
+    }
+
+    private inline fun showBiometricPrompt(
+        cipher: Cipher,
+        crossinline successAction: (Cipher?) -> Unit
+    ) {
+        createBiometricPrompt(successAction)
+            .authenticate(
+                createBiometricPromptInfo("", ""),
+                BiometricPrompt.CryptoObject(cipher)
+            )
+    }
+
+    private inline fun createBiometricPrompt(crossinline successAction: (Cipher?) -> Unit): BiometricPrompt {
+        return BiometricPrompt(this, object : BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                successAction(result.cryptoObject?.cipher)
+            }
+        })
+    }
+
+    private fun createBiometricPromptInfo(
+        title: String,
+        description: String
+    ): BiometricPrompt.PromptInfo {
+        return BiometricPrompt.PromptInfo.Builder().apply {
+            setTitle("Authenticate to restore saved data")
+            setAllowedAuthenticators(BIOMETRIC_STRONG or DEVICE_CREDENTIAL)
+        }.build()
     }
 }
--- app/src/main/java/com/stochanskyi/librariesdemo/presentaiton/feature/biometrics/BiometricsDemoViewModel.kt
+++ app/src/main/java/com/stochanskyi/librariesdemo/presentaiton/feature/biometrics/BiometricsDemoViewModel.kt
@@ -3,6 +3,8 @@ package com.stochanskyi.librariesdemo.presentaiton.feature.biometrics
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.stochanskyi.librariesdemo.presentaiton.utils.livedata.SingleLiveEvent
+import javax.crypto.Cipher
 import javax.inject.Inject
 
 class BiometricsDemoViewModel @Inject constructor() : ViewModel() {
@@ -10,11 +12,25 @@ class BiometricsDemoViewModel @Inject constructor() : ViewModel() {
     private val _restoredLiveData = MutableLiveData<String>()
     val restoredLiveData: LiveData<String> = _restoredLiveData
 
+    private val _authenticateEncryptCipherLiveData = SingleLiveEvent<Cipher>()
+    val authenticateEncryptCipherLiveData: LiveData<Cipher> = _authenticateEncryptCipherLiveData
+
+    private val _authenticateDecryptCipherLiveData = SingleLiveEvent<Cipher>()
+    val authenticateDecryptCipherLiveData: LiveData<Cipher> = _authenticateDecryptCipherLiveData
+
     fun saveData(data: String) {
 
     }
 
     fun restoreData() {
+
+    }
+
+    fun encodeData(cipher: Cipher) {
+
+    }
+
+    fun decodeData(cipher: Cipher) {
 
     }
 }
