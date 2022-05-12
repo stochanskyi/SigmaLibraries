eb92111e5e53633cfe285a13842c06d7d4ae2801
Marian Stochanskyi <stochan.andr@gmail.com>
2022-05-12 13:02:05 +0300

    Add firebase crashlytics

--- app/src/main/java/com/stochanskyi/librariesdemo/app/LibrariesDemoApp.kt
+++ app/src/main/java/com/stochanskyi/librariesdemo/app/LibrariesDemoApp.kt
@@ -3,6 +3,10 @@ package com.stochanskyi.librariesdemo.app
 import android.app.Application
 import android.content.Context
 import com.google.android.gms.maps.MapsInitializer
+import com.google.firebase.crashlytics.ktx.crashlytics
+import com.google.firebase.crashlytics.ktx.setCustomKeys
+import com.google.firebase.ktx.Firebase
+import com.stochanskyi.librariesdemo.BuildConfig
 import com.stochanskyi.librariesdemo.app.di.components.AppComponent
 import com.stochanskyi.librariesdemo.app.di.components.DaggerAppComponent
 import com.stochanskyi.librariesdemo.app.di.dependencies.AppDependencies
@@ -12,8 +16,16 @@ class LibrariesDemoApp : Application(), AppDependencies {
 
     lateinit var appComponent: AppComponent
 
+    override val context: Context
+        get() = this
+
     override fun onCreate() {
         super.onCreate()
+
+        Firebase.crashlytics.setCustomKeys {
+            key("build_type", BuildConfig.BUILD_TYPE)
+        }
+
         Timber.plant(Timber.DebugTree())
 
         Timber.d("AppStarted")
@@ -25,9 +37,6 @@ class LibrariesDemoApp : Application(), AppDependencies {
 
         MapsInitializer.initialize(applicationContext, MapsInitializer.Renderer.LATEST, null)
     }
-
-    override val context: Context
-        get() = this
 }
 
 fun Context.appComponent(): AppComponent {
